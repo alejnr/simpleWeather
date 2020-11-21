3fd0a30a2abc8df6d75bd4218bea73f6eaf3efc5
Alex Menlah <a.menlah@itebodoni.edu.it>
2020-11-21 16:57:50 +0100

    commit simple weather v2

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
 const express = require('express')
 const https = require('https')
 const dotenv = require('dotenv')
+const bodyParser = require('body-parser')
 
 dotenv.config()
 
 const app = express()
 
-app.get('/', function(req, res){
+app.use(bodyParser.urlencoded({ extended: true }))
 
-    const url = `https://api.openweathermap.org/data/2.5/weather?appid=${process.env.API_KEY}&q=parma&units=metric`
+const port = process.env.PORT
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/', function(req, res) {
+
+    const apiKEY = process.env.API_KEY
+    const query = req.body.cityName
+    const unit = 'metric'
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKEY}&q=${query}&units=${unit}`
 
     https.get(url, function(response) {
         
         response.on('data', function(data) {
+
+            function capitalizeFirstLetter(string) {
+                return string.charAt(0).toUpperCase() + string.slice(1);
+            }
 
             const weatherData = JSON.parse(data)
             const temp = weatherData.main.temp
@@ -21,7 +38,7 @@ app.get('/', function(req, res){
             const iconURL = 'http://openweathermap.org/img/wn/'
             const imageURL = iconURL + icon +  '@2x.png'
             
-            res.write('<h1>The temprature in Parma is ' + Math.floor(temp) + ' degree Celcius</h1>')
+            res.write('<h1>The temperature in ' + capitalizeFirstLetter(query) + ' is ' + Math.floor(temp) + ' degree Celcius</h1>')
             res.write('<p>The weather is currently ' + desc + '</p>')
             res.write('<img src='+ imageURL + '>')
             res.send()
@@ -33,6 +50,6 @@ app.get('/', function(req, res){
 })
 
 
-app.listen(process.env.PORT, function() {
-    console.log(`server is running on port ${process.env.PORT}`);
+app.listen(port, function() {
+    console.log(`server is running on port ${port}`);
 })
